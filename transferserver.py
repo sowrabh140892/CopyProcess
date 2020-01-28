b8e30a3fb4d328b71b3037fd73ba8b0626e04d60
sowrabh140892 <49732417+sowrabh140892@users.noreply.github.com>
2020-01-28 12:13:16 -0800

    Update transferserver.py

--- transferserver.py
+++ transferserver.py
@@ -4,20 +4,21 @@ import os
 from os import environ
 import re
 import logging
-import sys 
+import sys
 
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-S3_BUCKET_NAME = environ.get('S3_BUCKET_NAME')
-VFX_VENDOR_TEST = environ.get('VFX_VENDOR_TEST')
-KEY = str(sys.argv[1])
+source_bucket_key=str(sys.argv[1])
+destination_bucket_key=str(sys.argv[2])
+source_bucket_name=str(sys.argv[3])
+destination_bucket_name=str(sys.argv[4])
 aws_access_key_id=environ.get('aws_access_key_id')
 aws_secret_access_key=environ.get('aws_secret_access_key')
 session = boto3.Session(aws_access_key_id, aws_secret_access_key, region_name='us-east-1', )
 print dir(session)
 
-def copy_to_vfx_vendor_test(bucket_key=KEY, source_bucket_name=S3_BUCKET_NAME, destination_bucket_name=VFX_VENDOR_TEST):
+def copy_to_vfx_vendor_test(source_bucket_key, destination_bucket_key, source_bucket_name, destination_bucket_name):
     """
     :param bucket_key:
     :param source_bucket_name:
@@ -27,17 +28,18 @@ def copy_to_vfx_vendor_test(bucket_key=KEY, source_bucket_name=S3_BUCKET_NAME, d
     s3_resource = session.resource('s3')
     copy_source = {
         'Bucket': source_bucket_name,
-        'Key': bucket_key
+        'Key': source_bucket_key
     }
     bucket = s3_resource.Bucket(destination_bucket_name)
-    obj = bucket.Object(bucket_key)
-    print "Copying key %s" % bucket_key
+    obj = bucket.Object(destination_bucket_key)
+    print "Copying key %s" % source_bucket_key
     obj.copy(copy_source)
     print "Copy Completed!"
+
 def main():
     """
     :return:
     """
-    copy_to_vfx_vendor_test()
+    copy_to_vfx_vendor_test(source_bucket_key, destination_bucket_key, source_bucket_name, destination_bucket_name)
 if __name__ == '__main__':
     main()
